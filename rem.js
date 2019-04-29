0e60c768d72192067b6b16aa6d2acb4de2ef3759
zhaoshuai58 <zhaoshuai58@jd.com>
2019-04-29 14:17:24 +0800

    rem初始化

---
+++ rem.js
@@ -0,0 +1,15 @@
+(function (doc, win) {
+    var docEl = doc.documentElement,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+        recalc = function () {
+            var clientWidth = docEl.clientWidth;
+            if (!clientWidth) return;
+            if (clientWidth >= 640) {
+                clientWidth = 640;
+            }
+            docEl.style.fontSize = clientWidth * 100 / 375 + 'px';
+        };
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
